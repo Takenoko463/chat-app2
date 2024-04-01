b34d40e90f9d690590d3b88eb6974d256d2d2d82
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-01 17:00:50 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,26 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+
+  def create
+    # side_barから送られてきたparams params
+    @room = Room.find(params[:room_id])
+    # message tableに保存すべきparams message_params
+    @message = @room.messages.new(message_params)
+
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      # 失敗したら、indexアクションのpageへ
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
