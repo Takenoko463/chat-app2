52f4b35845eca6b5ed7cb6030662eec550d76b9d
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-01 17:14:44 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -14,6 +15,7 @@ class MessagesController < ApplicationController
       redirect_to room_messages_path(@room)
     else
       # 失敗したら、indexアクションのpageへ
+      @messages = @room.messages.includes(:user)
       render :index, status: :unprocessable_entity
     end
   end
